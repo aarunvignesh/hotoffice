051c4ede12dfb76bf45c5dc52ae4ac3bffbc8763
Vignesh, Arun <arun.vignesh@pearson.com>
2017-09-11 10:52:07 +0530

    Hiding header for login page

--- app/Core/Component/Header.component.ts
+++ app/Core/Component/Header.component.ts
@@ -1,22 +1,35 @@
-import {Component} from "@angular/core";
+import {Component,OnInit} from "@angular/core";
 import {loginService} from "./../../Login/Service/login.service";
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, ResolveEnd } from '@angular/router';
 
 @Component({
     selector:"wse-open-office-header",
     templateUrl:"./../Views/header.component.html"
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit{
 
     userData: any;
     userSubscription:any;
+    hideHeader: any;
 
-    constructor(private login: loginService, private router: Router){
+    constructor(private login: loginService, private router: Router, private route: ActivatedRoute){
         let self = this;
        this.userSubscription = this.login.announceSignIn.subscribe((user:any) => {self.userData=user});
        /*router.paramMap.subscribe((result: any)=>{
        		console.log(result);
        });*/
+    }
+
+    ngOnInit(){
+        var self = this;
+        this.router.events.subscribe((event)=>{
+            if(event instanceof ResolveEnd){
+            self.hideHeader = true;
+              if(event.state.url == "/login"){
+                self.hideHeader = false;
+              }
+            }
+        });
     }
 
     logout(){
